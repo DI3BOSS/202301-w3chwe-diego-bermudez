332df026c1f28a652d0ae413e3baa1e5263268db
DI3BOSS <diegofbermudez@gmail.com>
2023-01-29 19:32:12 +0100

    Show addtional information to cards

--- src/components/PokemonCardComponent/PokemonCardComponent.ts
+++ src/components/PokemonCardComponent/PokemonCardComponent.ts
@@ -3,6 +3,8 @@ import Component from "../Component/Component.js";
 class CardComponent extends Component {
   image: string;
   name: string;
+  id: number;
+  type: string;
 
   constructor(parentElement: Element) {
     super(parentElement, "pokedex--card card-list--card card", "li");
@@ -11,13 +13,16 @@ class CardComponent extends Component {
   getPokeInfo = async (number: number) => {
     const pokeApiUrl = "https://pokeapi.co/api/v2/pokemon/";
     const responseFromPokeApi = await fetch(`${pokeApiUrl}${number}`);
-    const pokemonImage = (await responseFromPokeApi.json()) as Record<
+    const pokemonInformation = (await responseFromPokeApi.json()) as Record<
       string,
       any
     >;
-    this.image = pokemonImage.sprites.versions["generation-v"]["black-white"]
-      .animated.front_default as string;
-    this.name = pokemonImage.name as string;
+    this.image = pokemonInformation.sprites.versions["generation-v"][
+      "black-white"
+    ].animated.front_default as string;
+    this.name = pokemonInformation.name as string;
+    this.id = pokemonInformation.id as number;
+    this.type = pokemonInformation.types[0].type.name as string;
 
     this.render();
   };
@@ -29,11 +34,15 @@ class CardComponent extends Component {
     this.domElement.innerHTML = `
     <img src="${this.image}" 
       class="card--pokemon-image
-      with="${imageWidth}" 
+      width="${imageWidth}" 
       height="${imageHeight}"
       alt="animation of ${this.name}">
 
-    <h2 class="card--pokemon-name">${this.name}</h3>`;
+    <h2 class="card--pokemon-name">${this.name}</h3>
+    <div class="card--pokemon-info"><span class="card--pokemon-id">#${this.id}</span> 
+    <span class="card--pokemon-type">Type: ${this.type}</span> </div>`;
+
+    this.domElement.classList.add(`pokemon-type-${this.type}`);
   }
 }
 
