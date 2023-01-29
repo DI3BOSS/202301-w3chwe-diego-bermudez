e2a7f97703ea63d2b5a44791394dc79c5d5b4c2c
DI3BOSS <diegofbermudez@gmail.com>
2023-01-29 13:23:56 +0100

    Implement a early list of 20 cards

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import CardComponent from "./components/CardComponent/CardComponent.js";
+import CardComponent from "./components/PokemonCardComponent/PokemonCardComponent.js";
 import CardListComponent from "./components/CardListComponent/CardListComponent.js";
 import HeaderComponent from "./components/HeaderComponent/HeaderComponent.js";
 import PageComponent from "./components/PageComponent/pageComponent.js";
@@ -12,5 +12,12 @@ headerContainer.render();
 const cardListComponent = new CardListComponent(mainContainer.domElement);
 cardListComponent.render();
 
-const cardComponent = new CardComponent(cardListComponent.domElement);
-await cardComponent.getPokeInfo(1);
+const numberOfPokeCardsByPage = 20;
+let cardIndexInPage = 0;
+const renderPokeCardsById = new Array(numberOfPokeCardsByPage).fill("");
+
+renderPokeCardsById.forEach(async () => {
+  await new CardComponent(cardListComponent.domElement).getPokeInfo(
+    cardIndexInPage++
+  );
+});
